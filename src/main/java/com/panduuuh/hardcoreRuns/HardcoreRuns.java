ae41aa13ea68b3ee005174973967e89ee8b5a4c0
marijay1 <marin.jayse@gmail.com>
2025-04-26 13:00:02 -0500

    Initial commit

---
+++ src/main/java/com/panduuuh/hardcoreRuns/HardcoreRuns.java
@@ -0,0 +1,17 @@
+package com.panduuuh.hardcoreRuns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HardcoreRuns extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
